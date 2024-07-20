7519770ba16a549dd84438cd51d9cc427b3d526e
summerssheep <20222027@itla.edu.do>
2024-07-19 23:54:39 -0400

    l

--- Aplicacion/obj/Debug/net6.0/Aplicación.AssemblyInfo.cs
+++ Aplicacion/obj/Debug/net6.0/Aplicación.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aplicación")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd241b5d7a58277ed7a0cb4bac6808b4a4795b8b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e042e0177c5ff27fef48657a09116f87cc2a1ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aplicación")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aplicación")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
